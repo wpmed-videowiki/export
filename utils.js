5856d27fc28efc9e5dc24d6f632bc0eed4a4f0b3
hassanamin994 <hassanmamin994@gmail.com>
2019-06-08 22:40:33 +0200

    Fix

--- utils.js
+++ utils.js
@@ -116,7 +116,7 @@ function getVideoNumberOfFrames(url, callback) {
 }
 
 function downloadMediaFile(url, destination, callback = () => {}) {
-  exec(`ffmpeg -y -i ${url} -c copy ${destination}`, (err, stdout, stderr) => {
+  exec(`ffmpeg -y -i ${url} -vcodec copy -acodec copy ${destination}`, (err, stdout, stderr) => {
     if (err) {
       return callback(err);
     }
