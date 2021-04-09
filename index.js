f737c6508e637d266a9975199d2cc920ff69407d
Osama-Hamed <osamahamedmohammed@gmail.com>
2021-04-09 14:31:21 +0200

    support more tts langs

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const fs = require('fs');
 const { exec } = require('child_process');
-const langs = ['en', 'hi', 'es', 'ar', 'ja', 'uk', 'fr', 'or', 'te', 'gu', 'bn', 'pa', 'sat', 'sv', 'it'];
+const langs = [
+  'en',
+  'hi',
+  'es',
+  'ar',
+  'ja',
+  'uk',
+  'fr',
+  'or',
+  'te',
+  'gu',
+  'bn',
+  'pa',
+  'sat',
+  'sv',
+  'it',
+  'in',
+  'kn',
+  'ml',
+  'ta',
+];
 const APP_DIRS = ['./tmp', './videos', './final'];
 
-// Create necessary file dirs 
-APP_DIRS.forEach(dir => {
+// Create necessary file dirs
+APP_DIRS.forEach((dir) => {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
-})
+});
 
-
-langs.forEach(function(lang, index) {
-  const command = `node_modules/pm2/bin/pm2 start worker.js -i 1 --name=videowiki_converter_${lang} -- ${lang}` 
+langs.forEach(function (lang, index) {
+  const command = `node_modules/pm2/bin/pm2 start worker.js -i 1 --name=videowiki_converter_${lang} -- ${lang}`;
   setTimeout(() => {
     console.log(command);
     exec(command, (err) => {
@@ -21,4 +40,4 @@ langs.forEach(function(lang, index) {
       }
     });
   }, index * 1500);
-})
+});
